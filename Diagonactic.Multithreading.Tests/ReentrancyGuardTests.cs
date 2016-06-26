a9c1ed031cf1e98d6907a082ab7ac455be3e9cfd
Matthew S. Dippel <matthew.dippel@gmail.com>
2016-06-26 17:18:05 -0400

    Imported ReentrancyGuard from TheMissingMethods - only class available in this revision

---
+++ Diagonactic.Multithreading.Tests/ReentrancyGuardTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Diagonactic.Multithreading.Tests
+{
+    [TestClass]
+    public class ReentrancyGuardTests
+    {
+        [TestMethod]
+        public void ValidateReentrancyPreventsReentrancyWithFuncVariant()
+        {
+            var reentrancyGuard = new ReentrancyGuard();
+            bool isInGuard = false;
+            bool executedGuardedCode = false;
+            using (ManualResetEvent nonReentrantCode = new ManualResetEvent(false))
+            using (ManualResetEvent nonReentrantCodeDone = new ManualResetEvent(false))
+            {
+                reentrancyGuard.IsReentrancyPrevented.Should().BeFalse();
+                var allowedThread = new Thread(() =>
+                                        {
+                                            ReentrancyGuard.CallReentrancySafe(reentrancyGuard, () =>
+                                                                               {
+                                                                                   nonReentrantCode.Set();
+                                                                                   isInGuard = true;
+                                                                                   nonReentrantCodeDone.WaitOne();
+                                                                                   isInGuard = false;
+                                                                                   executedGuardedCode = true;
+                                                                                   return executedGuardedCode;
+                                                                               }).Should().Be(ReentrancyGuard.ReentrancyCallResult.Success);
+                                            executedGuardedCode.ShouldBeEquivalentTo(true);
+
+                                        });
+                allowedThread.Start();
+                nonReentrantCode.WaitOne(TimeSpan.FromSeconds(1)).Should().BeTrue();
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    reentrancyGuard.IsReentrancyPrevented.Should().BeTrue();
+                    ReentrancyGuard.CallReentrancySafe(reentrancyGuard, () =>
+                                                       {
+                                                           throw new Exception("Reentered method despite guard being present");
+#pragma warning disable 162
+                                                           return true;
+#pragma warning restore 162
+                                                       }).ShouldBeEquivalentTo(ReentrancyGuard.ReentrancyCallResult.GuardBlocked);
+
+                }
+                nonReentrantCodeDone.Set();
+                allowedThread.Join();
+                reentrancyGuard.IsReentrancyPrevented.ShouldBeEquivalentTo(false);
+                executedGuardedCode.ShouldBeEquivalentTo(true);
+            }
+        }
+
+        [TestMethod]
+        public void ValidateReentrancyPreventsReentrancyWithActionVariant()
+        {
+            var reentrancyGuard = new ReentrancyGuard();
+            bool isInGuard = false;
+            bool executedGuardedCode = false;
+            using (ManualResetEvent nonReentrantCode = new ManualResetEvent(false))
+            using (ManualResetEvent nonReentrantCodeDone = new ManualResetEvent(false))
+            {
+                reentrancyGuard.IsReentrancyPrevented.Should().BeFalse();
+                var allowedThread = new Thread(() =>
+                                        {
+                                            ReentrancyGuard.CallReentrancySafe(reentrancyGuard, () =>
+                                                                               {
+                                                                                   nonReentrantCode.Set();
+                                                                                   isInGuard = true;
+                                                                                   nonReentrantCodeDone.WaitOne();
+                                                                                   isInGuard = false;
+                                                                                   executedGuardedCode = true;
+                                                                                   
+                                                                               }).Should().Be(true);
+                                            executedGuardedCode.ShouldBeEquivalentTo(true);
+
+                                        });
+                allowedThread.Start();
+                nonReentrantCode.WaitOne(TimeSpan.FromSeconds(1)).Should().BeTrue();
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    reentrancyGuard.IsReentrancyPrevented.Should().BeTrue();
+                    ReentrancyGuard.CallReentrancySafe(reentrancyGuard, (Action)(() =>
+                                                       {
+                                                           throw new Exception("Reentered method despite guard being present");
+                                                       })).ShouldBeEquivalentTo(false);
+
+                }
+                nonReentrantCodeDone.Set();
+                allowedThread.Join();
+                reentrancyGuard.IsReentrancyPrevented.ShouldBeEquivalentTo(false);
+                executedGuardedCode.ShouldBeEquivalentTo(true);
+            }
+
+
+        }
+    }
+}
---
+++ Diagonactic.Multithreading/ReentrancyGuard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Diagonactic.Multithreading
+{
+    /// <summary>
+    /// Class for handling methods or code blocks that cannot allow re-entrancy
+    /// </summary>
+    public class ReentrancyGuard
+    {
+        private const int ReentrancyPrevented = 1, ReentrancyAllowed = 0;
+
+        private int m_reentrancy = 0;
+
+        public enum ReentrancyCallResult
+        {
+            Unset = 0,
+            Success = 1,
+            Fail = 2,
+            GuardBlocked = 3,
+        }
+
+        public bool IsReentrancyPrevented => Interlocked.CompareExchange(ref m_reentrancy, ReentrancyPrevented, ReentrancyPrevented) == ReentrancyPrevented;
+
+        /// <summary>
+        /// Used at the opening of a reentrancy prevention block.  Sets guard to prevent reentrancy.
+        /// </summary>
+        /// <returns>When it's safe to enter the reentrancy prevented block, returns <see langword="true"/>; otherwise <see langword="false"/></returns>
+        public bool SetGuardAndCheckEntry()
+        {
+            return Interlocked.CompareExchange(ref m_reentrancy, ReentrancyPrevented, ReentrancyAllowed) == ReentrancyAllowed;
+        }
+
+        /// <summary>
+        /// Used after the reentrancy prevention block to reset and allow future callers to enter the method.  This should always be used in a <see langword="finally"/> block to ensure it is set.
+        /// </summary>
+        /// <remarks>Use <see cref="CallReentrancySafe"/> for a convenient method to implement this pattern.</remarks>
+        public void AllowReentrancy()
+        {
+            Interlocked.Exchange(ref m_reentrancy, ReentrancyAllowed);
+        }
+
+        /// <summary>
+        /// Executes <paramrf name="action"/> with reentrancy protection from <paramref name="guard"/>
+        /// </summary>
+        /// <param name="guard">The reentrancy guard to use to prevent reentrancy - normally a unique guard should be used for a unique call</param>
+        /// <param name="action">The action to execute (will throw exceptions if the action throws exceptions</param>
+        /// <returns>If the guard blocks the call, returns <see cref="ReentrancyCallResult.GuardBlocked"/>; otherwise returns the result of <paramref name="action"/> as <see cref="ReentrancyCallResult.Success"/> or <see cref="ReentrancyCallResult.Fail"/></returns>
+        public static ReentrancyCallResult CallReentrancySafe(ReentrancyGuard guard, Func<bool> action)
+        {
+            if (!guard.SetGuardAndCheckEntry())
+                return ReentrancyCallResult.GuardBlocked;
+
+            try
+            {
+                return action() ? ReentrancyCallResult.Success : ReentrancyCallResult.Fail;
+            }
+            finally
+            {
+                guard.AllowReentrancy();
+            }
+        }
+
+        public static bool CallReentrancySafe(ReentrancyGuard guard, Action action)
+        {
+            if (!guard.SetGuardAndCheckEntry())
+                return false;
+
+            try
+            {
+                action();
+                return true;
+            }
+            finally
+            {
+                guard.AllowReentrancy();
+            }
+        }
+    }
+}
