b042888230cf6e28abd851d84c7a6b0f5b8333bc
Matthew S. Dippel <matthew.dippel@gmail.com>
2017-09-15 22:50:35 -0400

    Add post-build to strong name and sign with Yubikey Code Signing Certificate

--- DiagonacticMultithreading/Properties/VersionInfo.cs
+++ DiagonacticMultithreading/Properties/VersionInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 
 // This should be the same version as below
-[assembly: AssemblyFileVersion("1.1.42.1")]
+[assembly: AssemblyFileVersion("1.1.0.44")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.0.42.1")]
+[assembly: AssemblyInformationalVersion("1.1.0.44")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.42.1")]
+[assembly: AssemblyInformationalVersion("1.1.0.44")]
 #endif
